b0346bf59f0c82d1e1fae0bc2d087b3eca7009e2
John FOLLY <follyjohn@outlook.fr>
2021-05-24 17:33:34 +0000

    first commit

---
+++ app/cartly/config/database.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+from sqlalchemy import create_engine
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://cartlyuser2:dbcarpass@localhost/cartly"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
